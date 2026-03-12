13ccdf056b3da4b74f6d16bd1279fb513e2b8cf1
RicJSO <202300157@estudantes.ips.pt>
2026-03-12 16:54:57 +0000

    -Nova migration CommentMentions table e relationships

---
+++ fluxnote-backend/Migrations/20260312165304_commentMentions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fluxnotebackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class commentMentions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommentMentions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommentMentions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CommentMentions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CommentMentions_DocumentComments_CommentId",
+                        column: x => x.CommentId,
+                        principalTable: "DocumentComments",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentMentions_CommentId",
+                table: "CommentMentions",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentMentions_UserId",
+                table: "CommentMentions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommentMentions");
+        }
+    }
+}
